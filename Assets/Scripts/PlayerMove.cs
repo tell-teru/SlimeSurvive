7f5964ad057f26abbfbc50c77eb7d88e71fba8e0
Mami Tsutae <sora.teru.96neko@gmail.com>
2022-09-14 17:47:44 +0900

    Player移動
    PlayerMoveスクリプトで前後左右に移動可能に

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("↑");
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("↓");
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("→");
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("←");
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W");
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("S");
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("D");
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("A");
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
     }
 }
