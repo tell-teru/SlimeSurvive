e32384687c89643558b78bcc6ef227149cc1bb69
Mami Tsutae <sora.teru.96neko@gmail.com>
2022-09-15 03:08:31 +0900

    スライム生成
    WAVE間のステージ上にランダム位置で４種類のスライム生成

---
+++ Assets/Scripts/SlimeCreater.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeCreater : MonoBehaviour
+{
+    public GameObject[] slimes;
+    int slimeNumber;
+
+    //public GameObject normalSlime;
+
+    int createNum = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Create()
+    {
+        Debug.Log("aaaaa");
+        for (int i = 0; i <= createNum; i++)
+        {
+            Debug.Log("bbbbb");
+
+            float x = Random.Range(100f, 900f);
+            float z = Random.Range(100f, 900f);
+
+            slimeNumber = Random.Range(0, slimes.Length);
+            transform.position = new Vector3(x, 0, z);
+            Instantiate(slimes[slimeNumber], transform.position, transform.rotation);
+
+        }
+    }
+}
