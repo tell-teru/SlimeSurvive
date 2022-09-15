90d50bbdfc03108ce92ed27d6a27442da50c4097
Mami Tsutae <sora.teru.96neko@gmail.com>
2022-09-15 15:06:31 +0900

    スライム拡大縮小
    敵に当たるとスケールが小さく
    スライムに当たるとスケールが大きくなる

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Vector3 slimeScale;  //①仮の変数宣言
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,25 +17,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        slimeScale = gameObject.transform.localScale; //◆現在の大きさを代入
     }
 
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Slime")
-        {
-            Debug.Log("接触");
-            Destroy(collision.gameObject);
-        }
-    }
+    //public void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Enemy")
+    //    {
+    //        slimeScale.x = slimeScale.x - 0.1f;  //②変数keroのx座標を1増やして代入
+    //        slimeScale.y = slimeScale.y - 0.1f;
+    //        slimeScale.z = slimeScale.z - 0.1f;
+
+    //        gameObject.transform.localScale = slimeScale; //③大きさに変数keroを代入
+
+    //        Debug.Log("接触");
+    //    }
+    //}
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log(" Hit ");
+        //Debug.Log(" Hit ");
         if(other.gameObject.tag == "Slime")
         {
-            Debug.Log("HIT");
+            slimeScale.x = slimeScale.x + 0.1f;  //②変数keroのx座標を1増やして代入
+            slimeScale.y = slimeScale.y + 0.1f;
+            slimeScale.z = slimeScale.z + 0.1f;
+
+            gameObject.transform.localScale = slimeScale; //③大きさに変数keroを代入
+
+            Debug.Log("+1");
             Destroy(other.gameObject);
+
+        }
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            slimeScale.x = slimeScale.x - 0.1f;  //②変数keroのx座標を1増やして代入
+            slimeScale.y = slimeScale.y - 0.1f;
+            slimeScale.z = slimeScale.z - 0.1f;
+
+            gameObject.transform.localScale = slimeScale; //③大きさに変数keroを代入
+
+            Debug.Log("-1");
+
         }
     }
 }
