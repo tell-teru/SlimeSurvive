330dfd2220b2984ade4b206865eba1c771623e9e
tell-teru <sora.teru.96neko@gmail.com>
2022-09-28 01:53:25 +0900

    敵の出現位置修正
    Playerの現在位置から半径〇〇の円周上に出現に変更

--- Assets/Scripts/EnemyCreater.cs
+++ Assets/Scripts/EnemyCreater.cs
@@ -7,37 +7,49 @@ public class EnemyCreater : MonoBehaviour
     public GameObject normalEnemy;
 
     float norEneTimer = 0f;
-    float norEneInterval = 3f;
+    float norEneInterval =　0.5f;
+
+    PlayerController playerController;
+    public GameObject player;
+
+    private Transform playerpos;
+    private Vector3 enepos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = player.gameObject.GetComponent<PlayerController>();
+        //playerpos = player.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         norEneTimer += Time.deltaTime;
+        playerpos = player.gameObject.transform;//中心座標
 
+
+        enepos = playerpos.position + CircleHorizon(100);// 半径()の円周上に出現
 
         if (norEneTimer >= norEneInterval)
         {
-            transform.position = new Vector3(Random.Range(200.0f, 250.0f), 0, Random.Range(200.0f, 400.0f));
+            transform.position = enepos;
+
             Instantiate(normalEnemy, transform.position, transform.rotation);
 
-            transform.position = new Vector3(Random.Range(350.0f, 400.0f), 0, Random.Range(200.0f, 400.0f));
-            Instantiate(normalEnemy, transform.position, transform.rotation);
-
-            transform.position = new Vector3(Random.Range(200.0f, 400.0f), 0, Random.Range(200.0f, 250.0f));
-            Instantiate(normalEnemy, transform.position, transform.rotation);
-
-            transform.position = new Vector3(Random.Range(200.0f, 400.0f), 0, Random.Range(350.0f, 400.0f));
-            Instantiate(normalEnemy, transform.position, transform.rotation);
-
-           
+            
             norEneTimer = 0;
         }
 
+    }
+
+
+    private Vector3 CircleHorizon(float radius)
+    {
+        var angle = Random.Range(0, 360);
+        var rad = angle * Mathf.Deg2Rad;
+        var px = Mathf.Cos(rad) * radius;
+        var pz = Mathf.Sin(rad) * radius;
+        return new Vector3(px, 0, pz);
     }
 }
