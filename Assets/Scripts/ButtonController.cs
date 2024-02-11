cd5cb1e179f75b67af3afa8b476626d1adc22a49
Mami Tsutae <sora.teru.96neko@gmail.com>
2024-02-11 23:09:36 +0900

    [add]  UI : ゲームスタートボタンの追加とボタン点滅仕様

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
@@ -9,16 +11,23 @@ public class ButtonController : MonoBehaviour
     [SerializeField]
     GameObject button;
 
+    private float initialAlpha = 0.2f;
+
+    // 点滅の間隔
+    [SerializeField]
+    private float blinkInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        initialAlpha = button.GetComponent<Image>().color.a;
+        StartBlinkAnimation();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnClickButtonSceneMove()
@@ -41,5 +50,13 @@ public class ButtonController : MonoBehaviour
         {
             SceneManager.LoadScene("Title");
         }
+    }
+
+    void StartBlinkAnimation()
+    {
+        // DOTweenを使用して透明度を変化させるアニメーションを作成
+        button.GetComponent<Image>().DOFade(0.2f, blinkInterval)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Yoyo);
     }
 }
