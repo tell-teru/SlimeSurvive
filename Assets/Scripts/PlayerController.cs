6f5d9a97f95803e7830b781b8881815d3192f97f
Mami Tsutae <sora.teru.96neko@gmail.com>
2024-02-12 18:09:20 +0900

    [fix] WAVEで敵に連続で当たりすぎると？体積が小さくなりすぎると？ 地面に埋まるバグを修正

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -86,6 +86,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+
         if (slimeScale == v0)
         {
             GameOver();
